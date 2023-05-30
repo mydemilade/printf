f7673471a2f5bc74c39c77a624d24ba367e4fd7e
mydemilade <ayunoluwademilade@gmail.com>
2023-05-29 17:43:16 -0700

    printf

--- printf.c
+++ printf.c
@@ -13,11 +13,12 @@ int _printf(const char *format, ...)
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
-	
+
 	if (format == NULL)
-		return (-1)
-			va_start(list, format);
-;
+		return (-1);
+
+	va_start(list, format);
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -32,14 +33,12 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-
-                        width = get_width(format, &i, list);
+			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-
-                                flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -60,8 +59,8 @@ int _printf(const char *format, ...)
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
+
 	*buff_ind = 0;
 }
